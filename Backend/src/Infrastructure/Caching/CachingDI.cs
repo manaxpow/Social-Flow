fda2f75d87dc29dc7fcf831dfe0f4964b4fc141d
namsama <trandandeptrai3que@gmail.com>
2026-08-22 17:24:20 +0700

    feat: add Aspire orchestration

--- Backend/src/Infrastructure/Caching/CachingDI.cs
+++ Backend/src/Infrastructure/Caching/CachingDI.cs
@@ -7,7 +7,7 @@ public static class CachingExtensions
     public static IServiceCollection AddCaching(this IServiceCollection services, IConfiguration configuration)
     {
 
-        var connectionString = configuration.GetConnectionString("Redis")
+        var connectionString = configuration.GetConnectionString("redis")
         ?? throw new ArgumentNullException("Connection string 'Redis' not found.");
         services.AddStackExchangeRedisCache(options =>
         {
